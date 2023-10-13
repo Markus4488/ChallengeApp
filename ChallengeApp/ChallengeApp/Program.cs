17f52c6178370a2989f06571ac931d051780282a
Markus4488 <markus4488@gmail.cim>
2023-10-13 22:52:34 +0200

    Zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,35 +1,16 @@
 ﻿string imie = "Ewa";
-char plec = 'm';
-int wiek = 44;
+char plec = 'k';
+int wiek = 33;
 
-// Pierwsza opcja:
 if (plec == 'k' && wiek < 30)
 {
-    Console.WriteLine("1.Kobieta poniżej 30 lat");
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
 if (imie == "Ewa" && wiek == 33)
 {
-    Console.WriteLine("1. Ewa, lat 33");
+    Console.WriteLine("Ewa, lat 33");
 }
 if (plec == 'm' && wiek < 18)
 {
-    Console.WriteLine("1. Niepełnoletni Mężczyzna");
-}
-
-
-// Druga opcja:
-if (plec == 'k')
-{
-    if (wiek < 30)
-    {
-        Console.WriteLine("2. Kobieta poniżej 30 lat");
-    }
-    if (wiek == 33)
-    {
-        Console.WriteLine("2. Ewa, lat 33");
-    }
-}
-else if (wiek < 18)
-{
-    Console.WriteLine("2. Niepełnoletni Mężczyzna");
+    Console.WriteLine("Niepełnoletni Mężczyzna");
 }
