640fd341ebebe4171d312722e1478952efdd156b
Markus4488 <markus4488@gmail.cim>
2023-10-13 22:10:53 +0200

    dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Ewa";
+char plec = 'm';
+int wiek = 44;
+
+// Pierwsza opcja:
+if (plec == 'k' && wiek < 30)
+{
+    Console.WriteLine("1.Kobieta poniżej 30 lat");
+}
+if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("1. Ewa, lat 33");
+}
+if (plec == 'm' && wiek < 18)
+{
+    Console.WriteLine("1. Niepełnoletni Mężczyzna");
+}
+
+
+// Druga opcja:
+if (plec == 'k')
+{
+    if (wiek < 30)
+    {
+        Console.WriteLine("2. Kobieta poniżej 30 lat");
+    }
+    if (wiek == 33)
+    {
+        Console.WriteLine("2. Ewa, lat  33");
+    }
+}
+else if (wiek < 18)
+{
+    Console.WriteLine("2. Niepełnoletni Mężczyzna");
+}
