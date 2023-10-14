472d2f8aca120ef7b2b3e157ee2c8dbbe9d22341
Markus4488 <markus4488@gmail.cim>
2023-10-14 18:17:49 +0200

    zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,18 @@
 ﻿int number = 4566;
 string numberInString = number.ToString(); //przekształcenie w zmienną typu string
 char[] letters = numberInString.ToArray(); //każda cyfra ze zmiennej numberInString wrzucona do osobnej, kolejnej komorki tablicy letters
-var licznik = 0;
+var counter = 0;
 
 
 for (int i = 0; i < 10; i++)
 {
-    foreach (char cyfra in letters)
+    foreach (char digit in letters)
     {
-        if (cyfra.ToString() == i.ToString())
+        if (digit.ToString() == i.ToString())
         {
-            licznik++;
+            counter++;
         }
     }
-    Console.WriteLine(i + "=> " + licznik);
-    licznik = 0;
+    Console.WriteLine(i + "=> " + counter);
+    counter = 0;
 }
